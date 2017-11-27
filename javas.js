15b0b0e6bb8caa459c92a82e4f588103499f07de
nicogimenez <nicogimenez@users.noreply.github.com>
2017-11-27 17:14:08 -0300

    Actualizacion

---
+++ javas.js
@@ -0,0 +1,247 @@
+$(document).ready(function(){
+	
+	
+	/*Entrevistas*/
+	
+$("#ritaboton").click(function(){
+	$("#sebaboton").hide();
+	$("#ritaboton").hide();
+	$("#fabianboton").hide();
+	$("#santiagoboton").hide();
+	$("#entrrita").fadeIn();
+	
+	
+	
+})
+	
+	
+	$("#sebaboton").click(function(){
+	$("#sebaboton").hide();
+	$("#ritaboton").hide();
+	$("#santiagoboton").hide();
+	$("#fabianboton").hide();
+	$("#entrseba").fadeIn();
+	
+	
+	
+})
+	
+	$("#fabianboton").click(function(){
+	$("#sebaboton").hide();
+	$("#ritaboton").hide();
+	$("#fabianboton").hide();
+	$("#santiagoboton").hide();
+	$("#entrfabian").fadeIn();
+		
+})
+	
+	$("#santiagoboton").click(function(){
+	$("#sebaboton").hide();
+	$("#ritaboton").hide();
+	$("#fabianboton").hide();
+	$("#santiagoboton").hide();
+	$("#entrsantiago").fadeIn();
+		
+})
+	
+	$("#entrseba").click(function(){
+	$("#sebaboton").fadeIn();
+	$("#ritaboton").fadeIn();
+	$("#santiagoboton").fadeIn();
+	$("#fabianboton").fadeIn();
+	$("#entrseba").hide();
+	
+	
+	
+})
+	
+	$("#entrrita").click(function(){
+	$("#sebaboton").fadeIn();
+	$("#ritaboton").fadeIn();
+	$("#santiagoboton").fadeIn();
+	$("#fabianboton").fadeIn();
+	$("#entrrita").hide();
+	
+	
+	
+})
+	
+	$("#entrfabian").click(function(){
+	$("#sebaboton").fadeIn();
+	$("#ritaboton").fadeIn();
+	$("#santiagoboton").fadeIn();
+	$("#fabianboton").fadeIn();
+	$("#entrfabian").hide();
+	
+	
+	
+})
+	
+	$("#entrsantiago").click(function(){
+	$("#sebaboton").fadeIn();
+	$("#ritaboton").fadeIn();
+	$("#santiagoboton").fadeIn();
+	$("#fabianboton").fadeIn();
+	$("#entrsantiago").hide();
+	
+	
+	
+})
+	
+	/*Conceptos*/
+	$(".close").click(function(){
+		$(".text").hide();
+		$(".close").hide();
+	})
+	
+	$("#btncomp").click(function(){
+		$(".text").hide();
+		$("#compt").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btncomptec").click(function(){
+		$(".text").hide();
+		$("#comptec").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btncompmet").click(function(){
+		$(".text").hide();
+		$("#compmet").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btncomppart").click(function(){
+		$(".text").hide();
+		$("#comppart").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btncomppers").click(function(){
+		$(".text").hide();
+		$("#comppers").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btnhab").click(function(){
+		$(".text").hide();
+		$("#hab").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btnvoc").click(function(){
+		$(".text").hide();
+		$("#voc").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btndes").click(function(){
+		$(".text").hide();
+		$("#des").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btndesw").click(function(){
+		$(".text").hide();
+		$("#desw").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btndis").click(function(){
+		$(".text").hide();
+		$("#dis").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btnhtml").click(function(){
+		$(".text").hide();
+		$("#html").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btncss").click(function(){
+		$(".text").hide();
+		$("#css").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btnjs").click(function(){
+		$(".text").hide();
+		$("#js").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btnresdes").click(function(){
+		$(".text").hide();
+		$("#resdes").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btnentlab").click(function(){
+		$(".text").hide();
+		$("#entlab").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btncv").click(function(){
+		$(".text").hide();
+		$("#cv").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btncarpre").click(function(){
+		$(".text").hide();
+		$("#carpre").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	$("#btnscr").click(function(){
+		$(".text").hide();
+		$("#scr").fadeIn();
+		$(".close").fadeIn();
+		
+		
+	})
+	
+	
+	
+	
+	
+	
+	
+})
